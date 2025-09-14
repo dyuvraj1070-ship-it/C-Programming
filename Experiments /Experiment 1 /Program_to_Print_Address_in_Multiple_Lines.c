3b7f8737b16a761cbea8347716794f3b9fed3bd1
Yuvraj Dhiman <dyuvraj1070@gmail.com>
2025-09-14 16:38:06 +0530

    new exp in 1

--- Experiments /Experiment 1 /Program_to_Print_Address_in_Multiple_Lines.c
+++ Experiments /Experiment 1 /Program_to_Print_Address_in_Multiple_Lines.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 int main () {
-    
-    printf("Yuvraj Dhiman\n")
-    printf("UPES\n")
-    printf("Dehradun\n")
-    printf("Programming\n")
 
+    printf("Yuvraj Dhiman\n");
+
+    printf("UPES\n");
+
+    printf("Dehradun\n");
+
+    printf("Programming\n");
+    
     return 0;
 }

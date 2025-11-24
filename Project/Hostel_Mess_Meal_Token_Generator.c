d8407ce50741e1bf48f80dbb4bbe74bbd4843021
Yuvraj Dhiman <dyuvraj1070@gmail.com>
2025-11-24 16:15:59 +0000

    token record

---
+++ Project/Hostel_Mess_Meal_Token_Generator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define FNAME "tokens.txt"
+#define NAME_LEN 50
+#define ROLL_LEN 20
+#define LINE_MAX 256
+#define MAX_DAILY_PER_STUDENT 2
+
+typedef enum { SLOT_MORNING = 1, SLOT_MIDDAY = 2, SLOT_EVENING = 3 } Slot;
+
+/* union for extra student info: store either phone OR allergy note (small) */
+typedef union {
+    char phone[16];
+    char allergy[30];
+} ExtraInfo;
+
+/* Token record kept in file and memory */
+typedef struct {
+    int tokenNo;
+    char date[11];          // YYYY-MM-DD
+    char roll[ROLL_LEN];
+    char name[NAME_LEN];
+    char meal[12];          // Breakfast/Lunch/Dinner
+    int slot;               // 1/2/3 for morning/midday/evening
+    int prefType;           // 0 = none, 1 = phone, 2 = allergy
+    ExtraInfo extra;        // union usage
+} Token;
